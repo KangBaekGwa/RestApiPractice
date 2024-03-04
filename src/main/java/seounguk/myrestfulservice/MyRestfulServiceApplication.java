afae89759ea52d587c5893e267aa9720a32ec2b7
KangBaekGwa <ksu9801@gmail.com>
2024-03-04 23:44:53 +0900

    fea : setting

---
+++ src/main/java/seounguk/myrestfulservice/MyRestfulServiceApplication.java
@@ -0,0 +1,21 @@
+package seounguk.myrestfulservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class MyRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+		String[] allBeanNames = ac.getBeanDefinitionNames();
+		for(String beanName : allBeanNames){
+			log.info("BeanName={}, {}",beanName, beanName.hashCode());
+		}
+	}
+
+}
